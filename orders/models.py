048b06f3bb6a92b55380418e70a70e247244510a
yami2587 <147910926+yami2587@users.noreply.github.com>
2025-08-03 18:04:07 +0530

    17780

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.user.username
