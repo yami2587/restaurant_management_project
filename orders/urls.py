d15d72587ac270b5e912b4b2302b82a3c58019f5
yami2587 <147910926+yami2587@users.noreply.github.com>
2025-08-06 19:58:15 +0530

    17781

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,25 @@
-from django.urls import path
-from .views import *
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Restaurent Menu</title>
+</head>
+<body>
+ <h1> Welecome to our Restaurent!</h1>
+ <div id="menu"></div>
+ <script>
+  fetch("/api/menu/")
+  .then(res => res.json())
+  .then(data =>{
+    document.getElementById('menu').innerHtml=data.map(
+        item => '<div><b>${item.name}</b>: ${item.price} <br>${item.description}<hr></div>').join('');
 
-urlpatterns = [
-    
-]
+    )
+  })
+  .catch(() => {
+    document.getElementById("menu").innerHtml = "Couldn't load menu";
+  });
+  </script>
+  </body>
+  </html>
+  
