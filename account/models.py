f48be630042486d186b9e56bff52bae527a2e191
yami2587 <147910926+yami2587@users.noreply.github.com>
2025-08-06 19:00:40 +0530

    17782

--- account/models.py
+++ account/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from restaurant.models import Menu
 
-# Create your models here.
+class Order(models.Model):
+    STATUS_CHOICE = [
+        ('pending','pending'),('confirmed','confirmed'),('delivered','delivered'),('cancelled','cancelled'),
+    ]
+    customer = models.ForeignKey(User,on_delete = models.CASCADE)
+    order_items = models.ManyToManyField(Menu)
+    total_amount = models.DecimalField(max_digits=8,decimal_places = 2)
+    status = models.CharField(max_length=20,choices = STATUS_CHOICE,default = 'pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Order #{self.id} by {self.customer.username}'
+
