1ce46f7e7edfef3e1c86fc600fda4a3cb7fc650d
yami2587 <147910926+yami2587@users.noreply.github.com>
2025-08-06 19:17:23 +0530

    17784

--- account/views.py
+++ account/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import api_view
+from resk_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def menu_view(request):
+    menu = [
+        {
+            'name':'Margherita Pizza',
+            'description':'Classic cheese pizza with basil'
+            'price':299
+        },
+        {
+            'name':'Pneer Tikka',
+            'description':'Classic grilld panner freshly spicy chilli and some juciy onion gravy ahhhhh'
+            'price':799
+        },
+        {
+            'name':'Gulab jamun',
+            'description':'Traditional gol gol kalle bolls with juciy suger syrup and optional - soaked with rum'
+            'price':99
+        }
+                
+    ]
+    return Response(menu) 
