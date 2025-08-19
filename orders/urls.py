036465fe2a40e4aabe1ba66fc3b663bd665d77a2
yami2587 <147910926+yami2587@users.noreply.github.com>
2025-08-19 22:01:21 +0530

    24012

--- orders/urls.py
+++ orders/urls.py
@@ -1,5 +1,13 @@
-from django.conf import settings
-from django.shortcuts import render
-def home_view(request):
-    restaurant_name = settings.RESTAURANT_NAME
-    return render(request , 'home.html',{'restaurant_name': restaurant_name})
+<!DOCKTYPE html>
+<html><head> 
+    <title>{{restaurant_name}}</title>
+    </head>
+<body style="font-family: arial; text-align:center;">
+    <h1 style="color: #333 ; margin-top: 50px">
+ Welcome to {{restaurant_name}}</h1>
+
+ <p style="color:#555 ; font-size: 10px;">
+    Delicious food served with love </p>
+
+ </body>
+</html>
