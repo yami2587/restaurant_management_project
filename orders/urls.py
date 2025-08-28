0b564874e55695b156381f38cae46346d08a98c7
yami2587 <147910926+yami2587@users.noreply.github.com>
2025-08-28 19:36:42 +0530

    35526

--- orders/urls.py
+++ orders/urls.py
@@ -2,12 +2,8 @@
 <html><head> 
     <title>{{restaurant_name}}</title>
     </head>
-<body style="font-family: arial; text-align:center;">
-    <h1 style="color: #333 ; margin-top: 50px">
- Welcome to {{restaurant_name}}</h1>
-
- <p style="color:#555 ; font-size: 10px;">
-    Delicious food served with love </p>
-
+<body>
+    <h1>Welcome to {{ restaurant_name }} </h1>
+    <p> We are happy to serve you delicious meals. </p>
  </body>
 </html>
