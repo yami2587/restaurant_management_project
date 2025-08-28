54f057dbe3f10c971b50995620f45b4e214edef0
yami2587 <147910926+yami2587@users.noreply.github.com>
2025-08-28 19:42:34 +0530

    35525

--- orders/urls.py
+++ orders/urls.py
@@ -5,5 +5,6 @@
 <body>
     <h1>Welcome to {{ restaurant_name }} </h1>
     <p> We are happy to serve you delicious meals. </p>
+    <p> <strong> Phone :</strong> {{restaurant_phone}} </p>
  </body>
 </html>
