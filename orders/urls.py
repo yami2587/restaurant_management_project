a500569238437f7efaae874e73a1e29624bb9208
yami2587 <147910926+yami2587@users.noreply.github.com>
2025-08-28 20:34:29 +0530

    37898

--- orders/urls.py
+++ orders/urls.py
@@ -5,9 +5,15 @@
      body {font-family: Arial, sans-serif; text-align: center; color:black;}
      h1{color: red; margin-top: 50px}
      p {font-size: 18px;margin:10px 0;}
+     img.logo{
+        width: 200px;
+        margin: 20px auto;
+        display: block;
+     }
     </head>
 <body>
     <div class="contact-box">
+     <img src= "{% static 'image/logo.png' % }" alt= "Logo" class = "logo">
      <h1> Contact Us </h1>
      <p> Email: support@demo.com </p>
      <p> Phone: +91 000000000</p>
