4704fc3a52381cef7950576d02eb4d575da500d6
yami2587 <147910926+yami2587@users.noreply.github.com>
2025-09-03 22:05:22 +0530

    37899

--- orders/urls.py
+++ orders/urls.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def menu_list(request):
-    menu_items = [
-        {"name": "Pizza","price":"250"},
-        {"name": "pasta","price":"450"},
-        {"name": "biryani","price":"650"},
-        {"name": "Coffe","price":"150"},
-    ]
-    return render(request , "menu_list.html" , {"menu_item": menu_items})
+    try:  
+        menu_items = [
+            {"name": "Pizza","price":"250"},
+            {"name": "pasta","price":"450"},
+            {"name": "biryani","price":"650"},
+            {"name": "Coffe","price":"150"},
+        ]
+        return render(request , "menu_list.html" , {"menu_item": menu_items})
+    except Exception as e:
+        error_message: f"Oops! somthing wrong : {str(e)}"
+        return HttpResponse(error_message,status=500)
